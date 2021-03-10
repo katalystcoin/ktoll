9a060adcb9b3e0418ebaae6d01095cd976431508
KatalystCoin <raymond@katalystcoin.com>
2021-03-11 04:03:41 +0800

    Update checkbalance.py

--- checkbalance.py
+++ checkbalance.py
@@ -1,23 +1,13 @@
-import requests
-from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+import pandas as pd
 
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
-page = requests.get(URL)
+url = 'https://etherscan.io/token/0xB8c77482e45F1F44dE1745F52C74426C631bDD52'
+req = Request(url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'})
 
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='ResultsContainer')
+response = urlopen(req, timeout=20).read()
+response_close = urlopen(req, timeout=20).close()
 
-# print(results.prettify())
-
-job_elems = results.find_all('section', class_='card-content')
-
-for job_elem in job_elems:
-    title_elem = job_elem.find('h2', class_='title')
-    company_elem = job_elem.find('div', class_='company')
-    location_elem = job_elem.find('div', class_='location')
-    if None in (title_elem, company_elem, location_elem):
-        continue
-    print(title_elem.text.strip())
-    print(company_elem.text.strip())
-    print(location_elem.text.strip())
-    print()
+page_soup = soup(response, "html.parser")
+balance = page_soup.find()
+print(page_soup)
